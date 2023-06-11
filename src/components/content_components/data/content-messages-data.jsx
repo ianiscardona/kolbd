ec471eb80dca10f1b98742ab4cb5a742d88a2537
Angelo Ian Michael Cardona <62699400+ianiscardona@users.noreply.github.com>
2023-06-11 12:17:58 +0800

    messages update

--- src/components/content_components/data/content-messages-data.jsx
+++ src/components/content_components/data/content-messages-data.jsx
@@ -31,10 +31,75 @@ const ContentMessagesData = [
   },
   {
     id: 5,
-    name: "Jericho",
-    nickname: "Echo",
-    teaser: "Swipe to open my message hehe",
-    message: "Good day!",
+    name: "Echo",
+    nickname: "",
+    teaser: "^_^",
+    message:
+      "NOPE BWISET!!!! i mean, happy bday din sayo. tnx for allowing me to collab with our bday. tnx 4 those times u listen abt my lyf and everything. long live also to ur relationship wt ur bf. and pls magbayad ka naman na utang u sakin be so pls stop being dramatic abt everything ",
+  },
+  {
+    id: 6,
+    name: "Che",
+    nickname: "",
+    teaser: "hbd",
+    message:
+      "Happy birthday, Koleng. I hope sana masaya and happy ang birthday mo. Thank you, Salamat for being our President👍👍👍",
+  },
+  {
+    id: 7,
+    name: "Rebs",
+    nickname: "",
+    teaser: ":D",
+    message:
+      "Happy birthday colleen, sana happy ka be. At maging masaya ka pa lalo. Beri gud ka blng prisidinti sana masstress ka pala lalu. Hbd, mbtc.",
+  },
+  {
+    id: 8,
+    name: "Jhoicy",
+    nickname: "",
+    teaser: "<3",
+    message:
+      "Happy Birthday ganda! Enjoy your day! Mahal ka namin, sana mabuhay ka pa ng super tagal lyk 100000 years pa. Labyu! ",
+  },
+  {
+    id: 9,
+    name: "CJ",
+    nickname: "",
+    teaser: "Hi be :*",
+    message:
+      "Hi be, Happy birthday. I wish na maging healthy and makapasa tayo lagi sa major kahit tatamad tamad tayo Hakhakhak. Thank you for inviting me rin sa bday mo. I'm happy na nakahanap ka na ng peace and love one na iniingatan ka talaga. Stay strong sa inyo. Also masaya ako na nakasama kita sa college journey ko. I wish you only the best. Smile and enjoy your day. Have a wonderful year! Happy new year! Char! Happy bday beeee! lab youuu 🥳🥳💖💖",
+  },
+  {
+    id: 10,
+    name: "𝓦𝓮𝓵𝓵𝓪",
+    nickname: "",
+    teaser: "wella",
+    message:
+      "Hi be, it's ur gal Wella. I just want to say that... You're doing great with every role you have, as a daughter, as a working-student, girlfriend, as a class representative, and especially as our friend. I would like to take this moment to congratulate you on your special day 🤗💖 and to say that I/we appreciate your efforts you're giving to us. I hope you'll enjoy your day and that your future would be full of 'Happy' Surprises (mag ka anak kana.. i meaaan). HAHAHAHA lablab 😘 U know n lag lng aq and2 handang makng if u hve problems🤗",
+  },
+  {
+    id: 11,
+    name: "Caleb",
+    nickname: "",
+    teaser: "B)",
+    message:
+      "Happy birthday! Today is a day to celebrate the wonderful person that you are and all the joy and love that you bring to those around you. May your day be filled with laughter, smiles, and all the things that make you happy. Here's to another year of making amazing memories and achieving your dreams. Cheers to you on your special day! ikaw na lang mag quilbot!!!",
+  },
+  {
+    id: 12,
+    name: "Ynyr",
+    nickname: "",
+    teaser: "ynyr",
+    message:
+      "Happy birthday sexbomb colleen! Sana maging close pa tayo lalo in the future. More gastos to come. Enjoy your birthday💐 xoxo lovelots~",
+  },
+  {
+    id: 13,
+    name: "Tantan",
+    nickname: "",
+    teaser: ">:)",
+    message:
+      "HBD beh. Sa totoo lang, di naman talaga ako babati pinilit lang kami Reby. Ayaw manahimik. Sana naman sa susunod na mga taon pa ng buhay mo, consider mo naman mag ayos ayos ng pag uugali. Pareho kayo ni Echo beh sa totoo lang. Anyway, mgpslmat k sa mga byaya sau ng panginoon at sa mgndang klusgan na meron k ngyun...,, kaarwn u 2day kya mgpslmat k ok..,,, tska 2mlong k nmn sa nres beh..,,, bka wnt u lng nmn...,,",
   },
 ];
 
