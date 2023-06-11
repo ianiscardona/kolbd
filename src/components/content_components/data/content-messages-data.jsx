47ee7e0fce04d49dfbf3a705556c3cad994304c2
Angelo Ian Michael Cardona <62699400+ianiscardona@users.noreply.github.com>
2023-06-12 00:03:34 +0800

    final update

--- src/components/content_components/data/content-messages-data.jsx
+++ src/components/content_components/data/content-messages-data.jsx
@@ -18,19 +18,37 @@ const ContentMessagesData = [
   {
     id: 3,
     name: "Kuya Jave",
-    nickname: "Echo",
-    teaser: "Swipe to open my message hehe",
-    message: "Hi be!",
+    nickname: "",
+    teaser: "happy bday ate",
+    message:
+      "happy happy birthday po sayo colleen slash ATE you are the generous people that ive known you've been our gem in the family. palage mn tyo mgtalo pero love ka parin ni kuya ikaw ang fave ko kaasaran all the time lalo na bardagulan. hahahaha 🤣 basta wag muna mgaasawa agh jowa po pede basta wag magmdali andyn lang si ian di aalis yan. hehe  basta as kuya ill protect u all sa harmful things . basta lge tandaan mkapagtapos para may diploma . okay syo n yang dream course ko😍 kaya keep it up and congrats in advance oky and welcome in reality world sna all you dreams do come true. DR. COLLEEN MERHAN . ILOVE YOU ATE COLLEEN .",
   },
   {
     id: 4,
     name: "Jaco",
-    nickname: "Jaco",
-    teaser: "Swipe to open my message hehe",
-    message: "Hi be!",
+    nickname: "jaco baho",
+    teaser: "hbd bro",
+    message:
+      "happy 21st birthday bro, wishing you good health and more birthdays to come. goodluck sa pag-aaral mo. sana wag ka magkaron ng back subjects, ayun lang enjoy your day labyu hehe.",
   },
   {
     id: 5,
+    name: "Tanya",
+    nickname: "",
+    teaser: "Happy birthday te",
+    message:
+      "Te ang tanda mo na 21 kana HAHAHAHHA Happy birthday sayooo thankyou sa lahat ng tulong mo samin naaapreciate namin yun  enjoy your day and we love you always pag need mo ng kausap just call tanya pretty😘",
+  },
+  {
+    id: 6,
+    name: "Chantal",
+    nickname: "chantal bantot",
+    teaser: "Happy birthday ate",
+    message:
+      "Happy Birthday ate colleen thankyou sa lahat lahat lalo na sa pag papautang sakin HAHAHAHAHA ingat ka palagi mahal na mahal ka namin enjoy ka ",
+  },
+  {
+    id: 7,
     name: "Echo",
     nickname: "",
     teaser: "^_^",
@@ -38,7 +56,7 @@ const ContentMessagesData = [
       "NOPE BWISET!!!! i mean, happy bday din sayo. tnx for allowing me to collab with our bday. tnx 4 those times u listen abt my lyf and everything. long live also to ur relationship wt ur bf. and pls magbayad ka naman na utang u sakin be so pls stop being dramatic abt everything ",
   },
   {
-    id: 6,
+    id: 8,
     name: "Che",
     nickname: "",
     teaser: "hbd",
@@ -46,7 +64,7 @@ const ContentMessagesData = [
       "Happy birthday, Koleng. I hope sana masaya and happy ang birthday mo. Thank you, Salamat for being our President👍👍👍",
   },
   {
-    id: 7,
+    id: 9,
     name: "Rebs",
     nickname: "",
     teaser: ":D",
@@ -54,7 +72,7 @@ const ContentMessagesData = [
       "Happy birthday colleen, sana happy ka be. At maging masaya ka pa lalo. Beri gud ka blng prisidinti sana masstress ka pala lalu. Hbd, mbtc.",
   },
   {
-    id: 8,
+    id: 10,
     name: "Jhoicy",
     nickname: "",
     teaser: "<3",
@@ -62,7 +80,7 @@ const ContentMessagesData = [
       "Happy Birthday ganda! Enjoy your day! Mahal ka namin, sana mabuhay ka pa ng super tagal lyk 100000 years pa. Labyu! ",
   },
   {
-    id: 9,
+    id: 11,
     name: "CJ",
     nickname: "",
     teaser: "Hi be :*",
@@ -70,15 +88,15 @@ const ContentMessagesData = [
       "Hi be, Happy birthday. I wish na maging healthy and makapasa tayo lagi sa major kahit tatamad tamad tayo Hakhakhak. Thank you for inviting me rin sa bday mo. I'm happy na nakahanap ka na ng peace and love one na iniingatan ka talaga. Stay strong sa inyo. Also masaya ako na nakasama kita sa college journey ko. I wish you only the best. Smile and enjoy your day. Have a wonderful year! Happy new year! Char! Happy bday beeee! lab youuu 🥳🥳💖💖",
   },
   {
-    id: 10,
-    name: "𝓦𝓮𝓵𝓵𝓪",
+    id: 12,
+    name: "Wella",
     nickname: "",
-    teaser: "wella",
+    teaser: "𝓦𝓮𝓵𝓵𝓪",
     message:
       "Hi be, it's ur gal Wella. I just want to say that... You're doing great with every role you have, as a daughter, as a working-student, girlfriend, as a class representative, and especially as our friend. I would like to take this moment to congratulate you on your special day 🤗💖 and to say that I/we appreciate your efforts you're giving to us. I hope you'll enjoy your day and that your future would be full of 'Happy' Surprises (mag ka anak kana.. i meaaan). HAHAHAHA lablab 😘 U know n lag lng aq and2 handang makng if u hve problems🤗",
   },
   {
-    id: 11,
+    id: 14,
     name: "Caleb",
     nickname: "",
     teaser: "B)",
@@ -86,7 +104,7 @@ const ContentMessagesData = [
       "Happy birthday! Today is a day to celebrate the wonderful person that you are and all the joy and love that you bring to those around you. May your day be filled with laughter, smiles, and all the things that make you happy. Here's to another year of making amazing memories and achieving your dreams. Cheers to you on your special day! ikaw na lang mag quilbot!!!",
   },
   {
-    id: 12,
+    id: 15,
     name: "Ynyr",
     nickname: "",
     teaser: "ynyr",
@@ -94,12 +112,75 @@ const ContentMessagesData = [
       "Happy birthday sexbomb colleen! Sana maging close pa tayo lalo in the future. More gastos to come. Enjoy your birthday💐 xoxo lovelots~",
   },
   {
-    id: 13,
+    id: 16,
     name: "Tantan",
     nickname: "",
     teaser: ">:)",
     message:
       "HBD beh. Sa totoo lang, di naman talaga ako babati pinilit lang kami Reby. Ayaw manahimik. Sana naman sa susunod na mga taon pa ng buhay mo, consider mo naman mag ayos ayos ng pag uugali. Pareho kayo ni Echo beh sa totoo lang. Anyway, mgpslmat k sa mga byaya sau ng panginoon at sa mgndang klusgan na meron k ngyun...,, kaarwn u 2day kya mgpslmat k ok..,,, tska 2mlong k nmn sa nres beh..,,, bka wnt u lng nmn...,,",
+  },
+  {
+    id: 17,
+    name: "Andeng",
+    nickname: "",
+    teaser: "<3<3<3",
+    message:
+      "Happy Birthday Coleng 😘💕 Enjoy your Day, Loveyou always takecare and stay what you are 👍😊 More Birthday's to come 😘",
+  },
+  {
+    id: 18,
+    name: "Cha",
+    nickname: "",
+    teaser: "cha",
+    message: "happy birthday, colleen! more birthdays to come! 🤍",
+  },
+  {
+    id: 19,
+    name: "Jerico",
+    nickname: "Ecolandia",
+    teaser: "hbd",
+    message:
+      "Happy Birthday Colleen, wish you goodhealth and sana maging successful kayo sabay ni ian, Stay strong sa inyong dalawa dapat walang titibag, more blessing to come🎉",
+  },
+  {
+    id: 20,
+    name: "Andrew",
+    nickname: "",
+    teaser: "salamat sa fuds",
+    message:
+      "happy birthday coleen ingatan mo sana ang bebe boy namin maging masaya ka sa buhay mo at maging successful sa napili mong career. alam naman naming iingatan ka nyan ni ian.  ps; sana pwede magbalot ng handa!!!  pss; shanghai at manok salamat.",
+  },
+  {
+    id: 21,
+    name: "Jhonrem",
+    nickname: "Jam",
+    teaser: "hbd haha",
+    message:
+      "happy birthday Colleen, stay strong sa inyo ni ian, i hope maging successful ang acads mo, sana lumaki kang isang mabuting tao, stay healthy and enjoy your day!!",
+  },
+  {
+    id: 22,
+    name: "Ichan",
+    nickname: "Di nakapunta",
+    teaser: "Hbd",
+    message:
+      "Happy Birthday Coleen! salamat at tinapos mo na ang pihikan phase ni Ian. Sensya na pass muna ako this time may trabaho kase. Enjoy your day at lasingin mo sila",
+  },
+  {
+    id: 23,
+    name: "Tams",
+    nickname: "",
+    teaser: "Happy birthday!",
+    message:
+      "Happy birthday, Colleen!💗 I wish you all the love and happiness. Sana matupad mo rin lahat ng pangarap mo at maging successful ka sa path na pipiliin mo. Stay strong sa inyo ni Ian kasi kapag hindi, hindi na kami maniniwala sa forever. Enjoy your special day and more birthdays to come! 🎉🎂 ",
+  },
+  {
+    id: 24,
+    name: "Gwenny",
+    nickname: "Tanggera",
+    teaser: "Happy birthday to you!",
+    message:
+      "Colleen! Happy birthday! 🎊🎉🎁 Alam mo ba ako pinaka mabait sa amin lahat, huwag ka magalala. pag nagkita tayo iseseminar kita lalo maging mabait. Enjoy and welcome sa tropahan! 💛💛💛",
   },
 ];
 
--- src/pages/remarks.jsx
+++ src/pages/remarks.jsx
@@ -4,13 +4,16 @@ export const Remarks = () => {
   return (
     <div className="relative flex flex-col items-center w-full overflow-hidden border-t-2 border-gray-900 bg-[#f5b498]">
       <h2 className="mt-5 mb-1 font-coming text-xl text-black">
-        Special Thanks and Happy Birthday to
+        Happy Birthday to
       </h2>
-      <h2 className="mb-1 font-coming font-bold text-3xl text-black">
+      <h2 className="mb-4 font-coming font-bold text-3xl text-black">
         Jericho!
       </h2>
       <h2 className="mb-10 font-coming text-xl text-black">
-        for gathering bday messages 📩
+        Thank you <span className="font-bold">Tanya</span> and{" "}
+        <span className="font-bold">Echo</span> for gathering messages and thank
+        you <span className="font-bold">everyone </span>
+        for the kind messages! 📩
       </h2>
     </div>
   );
